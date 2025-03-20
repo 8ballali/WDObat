cddd8d1baa26dead92d5f4cf64f1922763b52489
8ballali <iqbalalisaidil@gmail.com>
2025-03-20 12:54:56 +0700

    Pertemuan 2

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'nama' => 'Iqbal',
+                'alamat' => 'Jl Imam Bonjol',
+                'no_hp' => '090990909',
+                'role' => 'dokter',
+                'email' => 'iqbal@gmail.com',
+                'password' => 'password'
+            ],
+            [
+                'nama' => 'neena',
+                'alamat' => 'Jl Imam Bonjol',
+                'no_hp' => '09087878',
+                'role' => 'dokter',
+                'email' => 'neena@gmail.com',
+                'password' => 'password'
+            ],
+            [
+                'nama' => 'Budi',
+                'alamat' => 'Jl Imam Bonjol',
+                'no_hp' => '09087822278',
+                'role' => 'pasien',
+                'email' => 'budi@gmail.com',
+                'password' => 'password'
+            ],
+            [
+                'nama' => 'Doremi',
+                'alamat' => 'Jl Imam Bonjol10',
+                'no_hp' => '090878212278',
+                'role' => 'pasien',
+                'email' => 'doremi@gmail.com',
+                'password' => 'password'
+            ]
+            ];
+        foreach($data as $d){
+            User::create([
+                'nama' => $d['nama'],
+                'email' => $d['email'],
+                'password' => $d['password'],
+                'alamat' => $d['alamat'],
+                'no_hp' => $d['no_hp'],
+                'role' => $d['role']
+            ]);
+        }
+    }
+}
